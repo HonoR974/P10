f1377f34a9d4259ea0a154a15af0e50f274686a8
HonoR974 <honore.guillaudeau1@gmail.com>
2021-09-13 18:31:12 +0200

    Test Passed !
    Les tests passent

--- bibliotheque/src/main/java/com/bibliotheque/repository/BibliothequeRepository.java
+++ bibliotheque/src/main/java/com/bibliotheque/repository/BibliothequeRepository.java
@@ -4,10 +4,15 @@ import com.bibliotheque.model.Bibliotheque;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public interface BibliothequeRepository extends JpaRepository<Bibliotheque,Long> {
 
     Bibliotheque findById(long id);
 
     Bibliotheque findByNom(String nom);
+
+    List<Bibliotheque> findAll();
 }
--- bibliotheque/src/test/java/com/bibliotheque/service/BibliothequeServiceImplTest.java
+++ bibliotheque/src/test/java/com/bibliotheque/service/BibliothequeServiceImplTest.java
@@ -2,34 +2,20 @@ package com.bibliotheque.service;
 
 import com.bibliotheque.model.Bibliotheque;
 import com.bibliotheque.repository.BibliothequeRepository;
-
-
-import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 
@@ -54,12 +40,13 @@ public class BibliothequeServiceImplTest {
     @Before
     public void setUp()
     {
+        System.out.println("\n la classe before ");
         bibliothequeRepository = mock(BibliothequeRepository.class);
 
         list = new ArrayList<>();
 
         b1 = new Bibliotheque("B1", "rue 1");
-        b1 = new Bibliotheque("B2", "rue 2");
+        b2 = new Bibliotheque("B2", "rue 2");
 
         list.add(b1);
         list.add(b2);
@@ -67,49 +54,43 @@ public class BibliothequeServiceImplTest {
        initMocks(this);
 
         System.out.println("\n la liste " + list.toString());
-        Mockito.when(bibliothequeRepository.findAll()).thenReturn(list);
-        Mockito.when(bibliothequeRepository.findByNom("B1")).thenReturn(b1);
+        when(bibliothequeRepository.findAll()).thenReturn(list);
+        when(bibliothequeRepository.findByNom("B1")).thenReturn(b1);
         System.out.println("\n la liste findAll" + bibliothequeRepository.findAll());
     }
 
-
-    @Test
-    public void test()
-    {
-        List<Bibliotheque> listBiblio = bibliothequeRepository.findAll();
-
-        System.out.println("\n la liste des bibliotheque " + listBiblio.toString());
-
-        assertThat(listBiblio.size()).isEqualTo(2);
-
-        Bibliotheque bibliotheque = bibliothequeService.getByName("B1");
-        assertThat(bibliotheque.getNom()).isEqualTo("B1");
-    }
 
 
     @Test
     public void getAllBibliotheque  ()
     {
+        List<Bibliotheque> listBiblio = bibliothequeRepository.findAll();
+        System.out.println("\n la liste des bibliotheque " + listBiblio.toString());
 
+        assertThat(listBiblio.size()).isEqualTo(2);
     }
 
 
     @Test
     public void getBibliotheque()
     {
+        String name = "B1";
+        Bibliotheque bibliotheque = bibliothequeService.getByName(name);
+
+        assertThat(bibliotheque.getNom()).isEqualTo(name);
+    }
+
+    @Test
+    public void createBibliotheque()
+    {
 
     }
 
     @Test
-    void createBibliotheque() throws Exception {
-
+   public  void updateBibliotheque() {
     }
 
     @Test
-    void updateBibliotheque() {
-    }
-
-    @Test
-    void deleteBibliotheque() {
+    public void deleteBibliotheque() {
     }
 }
